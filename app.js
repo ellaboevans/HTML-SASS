aeffe66b9d549ef189b4a02c2608796d1cf8994a
eelabo <ellaboevans@gmail.com>
2022-04-15 02:04:21 +0000

    Fixed some bugs

--- app.js
+++ app.js
@@ -12,27 +12,7 @@ function PageTransitions(){
             this.className += ' active-btn';
         })
     }
-    //Sections Active
-    allSections.addEventListener('click', (e) =>{
-        const id = e.target.dataset.id;
-        if(id){
-
-            //remove selected from the other btns
-             SecBtns.forEach((btns) =>{
-                 btns.classList.remove('active')
-             })
-             e.target.classList.add('active')
-
-            //hide other sections
-            sections.forEach((section)=>{
-                section.classList.remove('active')
-            })
-
-            const element = document.getElementById(id);
-            element.classList.add('active');
-            
-        }
-    })
+   
 }
 
 
